32debf40c0fa3a205f46701ea9830f76302b91c7
sengmitnick <sengmitnick@163.com>
2022-12-05 09:51:53 +0800

    feat(admin): add proxy

--- apps/tower/vite.config.ts
+++ apps/tower/vite.config.ts
@@ -3,6 +3,15 @@ import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server: { port: 5173 },
+  base: '/admin/',
+  server: {
+    port: 5173,
+    proxy: {
+      '/api': {
+        target: process.env.API_URL,
+        changeOrigin: true,
+      },
+    },
+  },
   plugins: [react()],
 });
